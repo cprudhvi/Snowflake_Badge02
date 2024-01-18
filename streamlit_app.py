61cfa3ca3da82897f848f78e7dbfb58e6f7c0f2f
Prudhvi Cherukuri <46198561+cprudhvi@users.noreply.github.com>
2024-01-18 12:51:16 +0100

    Update streamlit_app.py
    added code to pull the data from s3 bucket csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Test application - Healthy Diner')
 
@@ -15,3 +16,5 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avovado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
