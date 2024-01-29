a9389b6e26ab78cf0009b3e7083c90236c2623cd
Prudhvi Cherukuri <46198561+cprudhvi@users.noreply.github.com>
2024-01-29 12:22:19 +0100

    Update streamlit_app.py
    Write Code to Add Rows to Our Fruit List in Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,3 +62,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add to the list?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
