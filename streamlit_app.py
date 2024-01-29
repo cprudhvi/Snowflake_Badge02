a19f8143055260532fceef3458ff623101801bbe
Prudhvi Cherukuri <46198561+cprudhvi@users.noreply.github.com>
2024-01-29 12:16:46 +0100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,4 +61,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add to the list?')
-steamlit.write('Thanks for adding ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
